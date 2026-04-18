6df274b11b2ced2d3bc8264f5696707f8ebd0bd3
Ben Masek <benmasek@Bens-MacBook-Air.local>
2026-04-17 18:20:14 -0700

    Render login page dynamically

--- apps/web/app/(marketing)/login/page.tsx
+++ apps/web/app/(marketing)/login/page.tsx
@@ -5,6 +5,8 @@ import { SiteHeader } from "../../../components/layout/site-header";
 import { isGoogleAuthEnabled } from "../../../lib/env";
 import { getCurrentUser } from "../../../server/auth";
 
+export const dynamic = "force-dynamic";
+
 export default async function LoginPage() {
   const user = await getCurrentUser();
 
